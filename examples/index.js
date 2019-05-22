424724c6deece0efb632c4340595a317d2d3771d
Light <pl2476@126.com>
2019-05-22 18:20:53 +0800

    add .eslintignore
    delete babel.config.js
    rebuild lib

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,9 @@
-const utils = require('../lib');
+import utils, { getTreeArr } from '../lib';
+
 const list = [
-  {id: 1, pid: 0, name: 11}, {id: 2, pid: 1, name: 2}
-]
-console.log(utils)
-console.log(utils.getTreeArr({ key: 'id', pKey: 'pid', data: list }))
+  { id: 1, pid: 0, name: 11 },
+  { id: 2, pid: 1, name: 2 },
+];
+
+console.log(utils);
+console.log(getTreeArr({ key: 'id', pKey: 'pid', data: list }));
